5baa3ad3e535ba9d447a8d636c7ab7d9b77dfb46
Fran Ribeiro <ribeirofrv@gmail.com>
2022-11-16 01:03:24 -0300

    challenge(9): query retorna o nome de todos os lanches que possuam calorias abaixo de 500

---
+++ challenges/desafio9.js
@@ -0,0 +1,11 @@
+// use("commerce");
+db.produtos
+  .find(
+    {
+      valoresNutricionais: {
+        $elemMatch: { tipo: "calorias", quantidade: { $lt: 500 } },
+      },
+    },
+    { _id: 0, nome: 1 },
+  )
+  .pretty();
