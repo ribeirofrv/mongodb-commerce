efef9592d637c6630568c3383809bb9389e258cc
Fran Ribeiro <ribeirofrv@gmail.com>
2022-11-15 23:48:35 -0300

    challenge(4): query retona produtos que tiveram vendas maiores que 50 e menores que 100

---
+++ challenges/desafio4.js
@@ -0,0 +1,12 @@
+// use("commerce");
+db.produtos
+  .find(
+    {
+      $and: [
+        { vendidos: { $gt: 50 } },
+         { vendidos: { $lt: 100 } },
+      ],
+    },
+    { _id: 0, nome: 1, vendidos: 1 },
+  )
+  .sort({ vendidos: 1 });
